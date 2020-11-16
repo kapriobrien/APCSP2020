0e57e8386ee2df9bc1ede0c1c28e5101e71eb56e
kapri <44206913+nitrospam@users.noreply.github.com>
2020-11-16 10:47:39 -0500

    Create pancakes.py

---
+++ CodeHS/Unit 1/1.5/pancakes.py
@@ -0,0 +1,19 @@
+def place_3_balls():
+    put_ball()
+    put_ball()
+    put_ball()
+    
+def move_twice():
+    move()
+    move()
+    
+move()
+place_3_balls()
+
+move_twice()
+place_3_balls()
+
+move_twice()
+place_3_balls()
+
+move()
