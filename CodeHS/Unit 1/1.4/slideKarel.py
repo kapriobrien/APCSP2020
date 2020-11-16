130ea515711a43a8ae2c55a5456d77db6630ae11
kapri <44206913+nitrospam@users.noreply.github.com>
2020-11-16 10:47:17 -0500

    Create slideKarel.py

---
+++ CodeHS/Unit 1/1.4/slideKarel.py
@@ -0,0 +1,20 @@
+def turnright():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+put_ball()
+
+turnright()
+
+move()
+turn_left()
+move()
+put_ball()
+move()
+
+turnright()
+
+move()
+put_ball()
+turn_left()
