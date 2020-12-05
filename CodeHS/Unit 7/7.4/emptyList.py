a678076caf63f750f0c31a655306ff55e8f07928
kapri <44206913+nitrospam@users.noreply.github.com>
2020-12-04 22:34:43 -0500

    Create emptyList.py

---
+++ CodeHS/Unit 7/7.4/emptyList.py
@@ -0,0 +1,14 @@
+# Enter your code here
+listList = []
+
+listList.append(3)   
+print(listList[0])
+
+listList.append('hello')
+print(listList[1])
+
+listList.append(False)
+print(listList[2])
+listList.remove(False)
+listList.remove(3)
+print(listList)
