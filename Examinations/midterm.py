c2dc60703600b62d9902371a2ffd5dd96fdca8f0
kapri <44206913+nitrospam@users.noreply.github.com>
2020-12-10 14:43:56 -0500

    Update midterm.py

--- Examinations/midterm.py
+++ Examinations/midterm.py
@@ -1,81 +1,88 @@
-rowOne = ['|',' ', "|",' ','|',' ','|' ]
-rowTwo = ['|',' ', "|",' ','|',' ','|' ]
-rowThree = ['|',' ', "|",' ','|',' ','|' ]
-columnOneScan = [rowOne[1],rowTwo[1],rowThree[1]]
-columnTwoScan = [rowOne[3],rowTwo[3],rowThree[3]]
-columnThreeScan = [rowOne[5],rowTwo[5],rowThree[5]]
-rowOneScan = [rowOne[1],rowOne[3],rowOne[5]]
-rowTwoScan = [rowTwo[1],rowTwo[3],rowTwo[5]]
-rowThreeScan = [rowThree[1],rowThree[3],rowThree[5]]
-diagonalOneScan = [rowOne[1],rowTwo[3],rowThree[5]]
-diagonalTwoScan = [rowOne[5],rowTwo[3],rowThree[1]]
+import sys
+
+boardDict = {
+            'A1': ' ', 'A2': ' ', 'A3': ' ',
+            'B1': ' ', 'B2': ' ', 'B3': ' ',
+            'C1': ' ', 'C2': ' ', 'C3': ' '
+            }
+
+def updateScanners():
+    global columnOneScan
+    global columnTwoScan
+    global columnThreeScan
+    global rowOneScan
+    global rowTwoScan
+    global rowThreeScan
+    global diagonalOneScan
+    global diagonalTwoScan
+    columnOneScan = [boardDict['A1'], boardDict['B1'], boardDict['C1']]
+    columnTwoScan = [boardDict['A2'], boardDict['B2'], boardDict['C2']]
+    columnThreeScan = [boardDict['A3'], boardDict['B3'], boardDict['C3']]
+    rowOneScan = [boardDict['A1'], boardDict['A2'], boardDict['A3']]
+    rowTwoScan = [boardDict['B1'], boardDict['B2'], boardDict['B3']]
+    rowThreeScan = [boardDict['C1'], boardDict['C2'], boardDict['C3']]
+    diagonalOneScan = [boardDict['A1'], boardDict['B2'], boardDict['C3']]
+    diagonalTwoScan = [boardDict['A3'], boardDict['B2'], boardDict['C1']]
+
+def boardPrint(board):
+    print('   1 2 3')
+    print('A |' + board['A1'] + '|' + board['A2'] + '|' + board['A3'] + '|')
+    print('  -------')
+    print('B |' + board['B1'] + '|' + board['B2'] + '|' + board['B3'] + '|')
+    print('  -------')
+    print('C |' + board['C1'] + '|' + board['C2'] + '|' + board['C3'] + '|')
+
+def checkRow(row): 
+  
+    win = True
+    point = turn
+    # Comparing each element with first item  
+    for point in row: 
+        if turn != point: 
+            win = False
+            break
+    
+    return win
+
+boardPrint(boardDict)
+
+for playerTurn in range(10):
+    if (playerTurn + 1)%2 == 0:
+        turn = 'O'
+    else:
+        turn = 'X'
+    point = str(input("Choose your point, " + turn + " | Format (A1,B2,C3): "))
+    if boardDict[point] == ' ':
+            boardDict[point] = str(turn)
+            boardPrint(boardDict)
+            updateScanners()
+    else:
+        print("That point is already taken. Turn Skipped")
+
+    if checkRow(columnOneScan):
+        print(turn + ' wins!')
+        sys.exit()
+    elif checkRow(columnTwoScan):
+        print(turn + ' wins!')
+        sys.exit()
+    elif checkRow(columnThreeScan):
+        print(turn + ' wins!')
+        sys.exit()
+    elif checkRow(rowOneScan):
+        print(turn + ' wins!')
+        sys.exit()
+    elif checkRow(rowTwoScan):
+        print(turn + ' wins!')
+        sys.exit()
+    elif checkRow(rowThreeScan):
+        print(turn + ' wins!')
+        sys.exit()
+    elif checkRow(diagonalOneScan):
+        print(turn + ' wins!')
+        sys.exit()
+    elif checkRow(diagonalTwoScan):
+        print(turn + ' wins!')
+        sys.exit()
 
 
-
-while True:
-    if turn == O:
-        if point == ('A',1) and rowOne[1] == ' ':
-            rowOne[1] = 'X'
-        elif point == ('A',2) and rowOne[3] == ' ':
-            rowOne[3] = 'X'
-        elif point == ('A',3) and rowOne[5] == ' ':
-            rowOne[5] = 'X'
-        elif point == ('B',1) and rowTwo[1] == ' ':
-            rowTwo[1] = 'X'
-        elif point == ('B',2) and rowTwo[3] == ' ':
-            rowTwo[3] = 'X'
-        elif point == ('B',3) and rowTwo[5] == ' ':
-            rowTwo[5] = 'X'
-        elif point == ('C',1) and rowThree[1] == ' ':
-            rowThree[1] = 'X'
-        elif point == ('C',2) and rowThree[3] == ' ':
-            rowThree[3] = 'X'
-        elif point == ('C',3) and rowThree[5] == ' ':
-            rowThree[5] = 'X'
-        else:
-            print("Choose an unoccupied space")
-            continue
-
-
-    if turn == X:
-        if point == ('A',1) and rowOne[1] == ' ':
-            rowOne[1] = 'O'
-            
-        elif point == ('A',2) and rowOne[3] == ' ':
-            rowOne[3] = 'O'
-        elif point == ('A',3) and rowOne[5] == ' ':
-            rowOne[5] = 'O'
-        elif point == ('B',1) and rowTwo[1] == ' ':
-            rowTwo[1] = 'O'
-        elif point == ('B',2) and rowTwo[3] == ' ':
-            rowTwo[3] = 'O'
-        elif point == ('B',3) and rowTwo[5] == ' ':
-            rowTwo[5] = 'O'
-        elif point == ('C',1) and rowThree[1] == ' ':
-            rowThree[1] = 'O'
-        elif point == ('C',2) and rowThree[3] == ' ':
-            rowThree[3] = 'O'
-        elif point == ('C',3) and rowThree[5] == ' ':
-            rowThree[5] = 'O'
-        else:
-            print("Choose an unoccupied space")
-            continue
-
-def playMove():
-    global point
-    global point
-    point = tuple(input("Choose your point | Format ('A',2): "))
-
-
-
-def updateGrid():
-    global realGrid
-    realGrid = str(rowOne) + '\n' + str(rowTwo) + '\n' + str(rowThree)
-
-def main():
-    print(grid)
-    #while not checkWinner('X') or not checkWinner('O'):
-    updateGrid()
-    print(realGrid)
-
-main()
+    
